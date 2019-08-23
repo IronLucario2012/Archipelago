21229c39c0a039652e17ad3faa5f58f21ce78b16
Jack <jackdavidhaseldine@hotmail.com>
2019-08-23 20:26:06 +0100

    Initial Game Files commit
    Adding the current game files to the repository.

---
+++ Assets/Scripts/GlobalVariablesGame.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum Direction { Up, Right, Down, Left };
+
+public class GlobalVariablesGame : MonoBehaviour {
+
+    public int levelNumber = 0;
+    public Level currentLevel;
+
+    private void Start()
+    {
+        levelNumber = GlobalVariablesMenu.levelNumber;
+        currentLevel = GlobalVariablesMenu.levelList.levels[levelNumber+1];
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("LevelSelectScene");
+    }
+
+
+}
+
+#region Level Data Classes
+[Serializable]
+public class LevelList
+{
+    public List<Level> levels;
+
+    public LevelList()
+    {
+        levels = new List<Level>();
+    }
+}
+
+[Serializable]
+public class Level
+{
+    public int id;
+    public int[] sizeXY;
+    public List<Node> nodes;
+
+    public Level(int[] size)
+    {
+        sizeXY = size;
+    }
+
+    public Level(int ID, int[] size, List<Node> list)
+    {
+        id = ID;
+        sizeXY = size;
+        nodes = list;
+    }
+
+    public void AddNode(Node n)
+    {
+        nodes.Add(n);
+    }
+
+    override
+    public string ToString()
+    {
+        string result = "";
+        result += "Width: " + sizeXY[0] + ", Height: " + sizeXY[1];
+        result += "\nNodes:";
+        foreach(Node n in nodes)
+        {
+            result += "\n" + n.ToString();
+        }
+
+        return result;
+    }
+}
+
+[Serializable]
+public class Node
+{
+    public int[] xy;
+    public int[] connections;
+
+    public Node(int[] positionXY, int[] connectionsNESW)
+    {
+        xy = positionXY;
+        connections = connectionsNESW;
+    }
+
+    override
+    public string ToString()
+    {
+        string result = "";
+
+        result += "Coordinates: (" + xy[0] + "," + xy[1] + ")\n";
+        result += "Number of connections north: " + connections[0] + ", east: " + connections[1] + ", south: " + connections[2] + ", and west: " + connections[3];
+
+
+        return result;
+    }
+}
+#endregion
---
+++ Assets/Scripts/GlobalVariablesMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GlobalVariablesMenu : MonoBehaviour {
+
+    public static int levelNumber;
+    public static LevelList levelList;
+
+	public void LoadLevel(Button button)
+    {
+        levelNumber = int.Parse(button.GetComponentInChildren<Text>().text);
+
+        Debug.Log("Loading level " + levelNumber);
+
+        SceneManager.LoadScene("GameScene");
+    }
+
+    private void OnEnable()
+    {
+        levelNumber = 0;
+        TextAsset levelData = (TextAsset)Resources.Load("Levels");
+        string jsonLevelString = levelData.text;
+        levelList = JsonUtility.FromJson<LevelList>(jsonLevelString);
+        Debug.Log(levelList.levels[0].ToString());
+    }
+}
